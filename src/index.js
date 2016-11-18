971bcbcee50c77e2216810f27f1df2313a4450c7
Colten Rouska <colten.rouska@extendhealth.com>
2016-11-18 11:38:01 -0700

    Basic functions added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-'use strict';
+import _ from 'lodash';
+
+function upperSnake(varr){
+  return _.snakeCase(varr).toUpperCase();
+}
+
+const abode = {
+  get(envVar){
+    return process.env[upperSnake(envVar)];
+  },
+  set(envVar, variable){
+    return process.env[upperSnake(envVar)] = variable;
+  },
+  default(envVar, defaultVar){
+    return abode.exists(envVar) ? abode.get(envVar) : abode.set(envVar, defaultVar);
+  },
+  exists(envVar){
+    return !!abode.get(envVar);
+  },
+  isEnv(env){
+    return _.isEqual(process.env.NODE_ENV, env);
+  }
+};
+
+export default abode;
--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,63 @@
-'use strict';
+import { expect } from 'chai';
+import abode from '../src';
+
+describe('abode', () =>{
+
+  describe('get', () =>{
+    it('can camelcase env variables', () =>{
+      process.env.HA_HA = 'worked';
+      expect(abode.get('haHa')).to.equal('worked');
+    });
+
+    it('returns undefined for unfound variable', ()=>{
+      expect(abode.get('SHOULD_NOT_EXIST')).to.equal(undefined);
+    });
+  });
+
+  describe('set', () =>{
+    it('can set the environment variable', () =>{
+      abode.set('haHa', '12345');
+      expect(process.env).to.have.property('HA_HA').and.to.equal('12345');
+    });
+
+    it('replaces an already defined environment variable', () =>{
+      process.env.HA_HA = 'yeah ok';
+      abode.set('haHa', 'ok yeah');
+      expect(process.env).to.have.property('HA_HA').and.to.equal('ok yeah');
+    })
+  });
+
+  describe('default', () =>{
+    it('does not replace the env variable if it exists', () =>{
+      process.env.HA_HA = 'not funny';
+      abode.default('haHa', 'very funny');
+      expect(process.env).to.have.property('HA_HA').and.to.equal('not funny');
+    });
+
+    it('sets the env variable if it does not exist', () =>{
+      abode.default('notHa', 'not funny');
+      expect(process.env).to.have.property('NOT_HA').and.to.equal('not funny');
+    });
+  });
+
+  describe('exists', () =>{
+    it('returns true if the variable exists', () =>{
+      process.env.TEST_VAR = 'yay';
+      expect(abode.exists('TEST_VAR')).to.be.true;
+    });
+
+    it('returns false if the variable does not exist', () =>{
+      expect(abode.exists('nope')).to.be.false;
+    });
+  });
+
+  describe('isEnv', () =>{
+    it('returns true if NODE_ENV equals test', () =>{
+      expect(abode.isEnv('test')).to.be.true;
+    });
+
+    it('returns false if NODE_ENV equals local', () =>{
+      expect(abode.isEnv('local')).to.be.false;
+    });
+  });
+});
